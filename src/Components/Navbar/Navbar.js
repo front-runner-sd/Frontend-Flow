d09d3696e438a601c8ed1d89e7f7ebc4138a0bb0
praptisharma28 <123169861+praptisharma28@users.noreply.github.com>
2023-10-02 20:18:19 +0530

    Task 3: Add stylized Button #3 issue resolved
    Added the stylized offset Contribute button as per given screenshots. Hope you like it.

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,23 +1,23 @@
 import styles from './Navbar.module.css';
 import logo from './Logo.png';
 function Navbar() {
-  return (
-    <nav>
-        <div>
-            <img src={logo} className={styles.Logo}/>
-        </div>
-        <div>
-            <ul className={styles.NavItems}>
-                <li><a>Home</a></li>
-                <li><a>Resources</a></li>
-                <li><a>About</a></li>
-            </ul>
-        </div>
-        <div >
-            <button className={styles.ContributeButton}>Contribute</button>
-        </div>
-    </nav>
-  );
+    return (
+        <nav>
+            <div>
+                <img src={logo} className={styles.Logo} />
+            </div>
+            <div>
+                <ul className={styles.NavItems}>
+                    <li><a>Home</a></li>
+                    <li><a>Resources</a></li>
+                    <li><a>About</a></li>
+                </ul>
+            </div>
+            <div>
+                    <button className={styles.button}>Contribute</button>
+            </div>
+        </nav >
+    );
 }
 
 export default Navbar;
